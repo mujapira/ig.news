2252ffdcc31051b9c1c3829263526a26784b9330
mujapiraa2w <mauricio@a2web.com.br>
2022-04-10 22:07:46 -0300

    Fauna e Stripe

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from "faunadb"
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -8,9 +11,43 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
         params: {
-          scope: "read:user"
+          scope: "read:user, user:email"
         },
       },
     }),
   ],
-})
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const email = user.email
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection("users"),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    },
+  },
+},
+)
